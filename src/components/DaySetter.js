a82cb171df660577d69daa8251ed9dcdea6a3983
Del Cudjoe <ervin.cudjoe@duke.edu>
2022-10-14 02:52:08 -0400

    pruned package.json

--- src/components/DaySetter.js
+++ src/components/DaySetter.js
@@ -32,6 +32,17 @@ const DaySetter = ({data, setData, day}) => {
     if (data[day].active) return value;
     return '';
   }
+  const toInputTimeString = (time) => {
+    let minutes = time / 60000;
+    let hours = 0;
+    while (minutes >= 60) {
+      minutes -= 60;
+      hours++;
+    }
+    if (minutes === 0) minutes = '00';
+    if (hours < 10) hours = '0' + hours.toString();
+    return hours + ':' + minutes;
+  }
   if (data === null) {
     return (
       <tr>
@@ -46,8 +57,8 @@ const DaySetter = ({data, setData, day}) => {
     <tr>
       <td>{day}:</td>
       <td><input type='checkbox' onChange={onActiveChange} checked={data[day].active}/></td>
-      <td><input type='time' disabled={!data[day].active} value={offOn(toTimeString(data[day].start))} onChange={x => onStartChange(x.target.value)}/></td>
-      <td><input type='time'disabled={!data[day].active} value={offOn(toTimeString(data[day].end))} onChange={x => onEndChange(x.target.value)}/></td>
+      <td><input type='time' disabled={!data[day].active} value={offOn(toInputTimeString(data[day].start))} onChange={x => onStartChange(x.target.value)}/></td>
+      <td><input type='time'disabled={!data[day].active} value={offOn(toInputTimeString(data[day].end))} onChange={x => onEndChange(x.target.value)}/></td>
     </tr>
     
   )
